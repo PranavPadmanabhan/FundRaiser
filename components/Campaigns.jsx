39ede0efa26cf4ec2ccee219608efc0eebad9c8a
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2022-08-20 16:29:47 +0530

    first commit

--- components/Campaigns.jsx
+++ components/Campaigns.jsx
@@ -7,7 +7,7 @@ const Campaigns = () => {
   const router = useRouter();
 
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 place-content-start lg:place-content-center place-items-center w-screen min-h-screen bg-black scrollbar-hide pt-[20%] sm:pt-[5%] lg:pt-0'>
+    <div className='grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 place-content-start lg:place-content-center place-items-center w-screen min-h-screen bg-black scrollbar-hide pt-[20%] sm:pt-[5%] lg:pt-[10%]'>
         <CampaignCard image={'/Assets/campaign.png'} amount={'100 Matic'} title={'Need fund for Cancer treatment'} date={'17/12/2022'} buttonTitle={'Go to Campaign'} onClick={() => router.push('/campaigns/1')}/>
         <CampaignCard image={'/Assets/campaign.png'} amount={'100 Matic'} title={'Need fund for Cancer treatment'} date={'17/12/2022'} buttonTitle={'Go to Campaign'} onClick={null}/>
         <CampaignCard image={'/Assets/campaign.png'} amount={'100 Matic'} title={'Need fund for Cancer treatment'} date={'17/12/2022'} buttonTitle={'Go to Campaign'} onClick={null}/>
--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -3,7 +3,7 @@ import CampaignCard from './CampaignCard'
 
 const Dashboard = () => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 place-content-start lg:place-content-center place-items-center w-screen min-h-screen bg-black scrollbar-hide pt-[20%] sm:pt-[5%] lg:pt-0'>
+    <div className='grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 place-content-start lg:place-content-center place-items-center w-screen min-h-screen bg-black scrollbar-hide pt-[20%] sm:pt-[5%] lg:pt-[10%]'>
         <CampaignCard image={'/Assets/campaign.png'} amount={'100 Matic'} title={'Need fund for Cancer treatment'} date={'17/12/2022'}  buttonTitle={'Close Campaign'} onClick={null}/>
         <CampaignCard image={'/Assets/campaign.png'} amount={'100 Matic'} title={'Need fund for Cancer treatment'} date={'17/12/2022'}  buttonTitle={'Close Campaign'} onClick={null}/>
         <CampaignCard image={'/Assets/campaign.png'} amount={'100 Matic'} title={'Need fund for Cancer treatment'} date={'17/12/2022'}  buttonTitle={'Close Campaign'} onClick={null}/>
